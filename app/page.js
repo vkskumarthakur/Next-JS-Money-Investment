fd7106964759a20c9b4565f9976efeb58f62737c
vikas <vkskumarthakur@gmail.com>
2024-07-19 01:26:51 +0530

    welcome message and button added on home screen

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from 'next/link';
 import MainLayout from "@/components/MainLayout";
 export default function Home() {
   return (
-    <div className="flex items-center justify-center" style={{minHeight:`calc(100vh - 65px)`}}>
-      this is home page
+    <div className="flex items-center flex-col justify-center gap-3" style={{minHeight:`calc(100vh - 65px)`}}>
+     <h1 className="text-[#d5ca8a]">Welcome to Dashboard</h1>
+     <Link href='/dashboard' title='GO'><button className="py-2 px-5 rounded-sm bg-white text-black">Start</button></Link>
     </div>
   );
 }
